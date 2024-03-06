16c0d1a1e5e3e7a1a2010467f40eea22593986de
Saufi <msaufi2325@gmail.com>
2024-03-07 00:37:19 +0900

    first commit

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/msaufi2325/test_connect
+
+go 1.22.0
---
+++ main.go
@@ -0,0 +1,23 @@
+package main
+
+func main() {
+	// connect to the database
+
+	// test my connection
+
+	// get rows from table
+
+	// insert a row
+
+	// get rows from table again
+
+	// update a row
+
+	// get rows from table again
+
+	// get one row by id
+
+	// delete a row
+
+	// get rows again
+}
