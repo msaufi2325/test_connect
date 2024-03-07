4402a302cedec84962614282aa223e8e6138e458
Saufi <msaufi2325@gmail.com>
2024-03-07 20:51:43 +0900

    Add dependencies and establish database connection

--- main.go
+++ main.go
@@ -1,9 +1,30 @@
 package main
 
+import (
+	"database/sql"
+	"fmt"
+	"log"
+
+	_ "github.com/jackc/pgx/v5/stdlib"
+)
+
 func main() {
 	// connect to the database
+	conn, err := sql.Open("pgx", "host=localhost port=5433 dbname=test_connect user=postgres password=password")
+	if err != nil {
+		log.Fatal(fmt.Errorf("error opening database connection: %v", err))
+	}
+	defer conn.Close()
+
+	log.Println("Connected to the database")
 
 	// test my connection
+	err = conn.Ping()
+	if err != nil {
+		log.Fatal(fmt.Errorf("error pinging database: %v", err))
+	}
+
+	log.Println("Pinged the database")
 
 	// get rows from table
 
